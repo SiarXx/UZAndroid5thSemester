ff509a1c3868a6e43e8783ed5fae386b1099d3ce
Przemek <mrfros22@gmail.com>
2019-12-12 00:28:59 +0100

    reminders layout

--- app/src/main/java/com/example/uzapp/fragments/Reminders.kt
+++ app/src/main/java/com/example/uzapp/fragments/Reminders.kt
@@ -6,21 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.example.uzapp.R
+import kotlinx.android.synthetic.main.fragment_reminders.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class Reminders : Fragment() {
+class Reminders : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reminders, container, false)
+        val view = inflater.inflate(R.layout.fragment_reminders, container, false)
+        val addReminderBtn = view.addReminder
+        addReminderBtn.setOnClickListener(this)
+        return view
     }
-
+    override fun onClick(view: View?) {
+        when(view!!.id) {
+            R.id.addReminder -> Navigation.findNavController(view).navigate(R.id.action_reminders_to_reminderView)
+            //R.id.deleteNotes -> deleteStart()
+            R.id.backNotes -> Navigation.findNavController(view).popBackStack()
+        }
+    }
 
 }

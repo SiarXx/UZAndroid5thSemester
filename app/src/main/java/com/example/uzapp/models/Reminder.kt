78692412198ad6a30f509a6431e30654f9754b97
Przemek <mrfros22@gmail.com>
2019-12-11 23:42:17 +0100

    reminders layout

--- app/src/main/java/com/example/uzapp/models/Reminder.kt
+++ app/src/main/java/com/example/uzapp/models/Reminder.kt
@@ -1,9 +1,11 @@
 package com.example.uzapp.models
 
 import java.io.Serializable
+import java.util.*
 
 data class Reminder(
     val reminderTitle:String? = null,
     val reminderBody: String? = null,
+    val reminderDate: Date,
     var selected: Boolean = false
 ):Serializable
